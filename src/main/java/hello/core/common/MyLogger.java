cb603e9312971c4c2805c414d66ca0edb8e13874
Psoyeon1117 <namhui271@gmail.com>
2026-03-30 22:29:15 +0900

    spring basic end 20260330

--- src/main/java/hello/core/common/MyLogger.java
+++ src/main/java/hello/core/common/MyLogger.java
@@ -3,12 +3,15 @@ package hello.core.common;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
 @Component
-@Scope(value = "request")
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
+//가짜 proxy 객체를 컨테이너에 등록함으로써 DI을 활용할 수 있다.
+//덕분에 Provider코드를 줄일 수 있다.
 public class MyLogger {
 
     private String uuid;
